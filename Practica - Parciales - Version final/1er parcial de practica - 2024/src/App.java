7f62f95e82d3d692558788fe2d6d3e8afc9a67b1
Nirael312 <arielzerna4@gmail.com>
2024-10-04 13:10:01 -0300

    123

--- Practica - Parciales - Version final/1er parcial de practica - 2024/src/App.java
+++ Practica - Parciales - Version final/1er parcial de practica - 2024/src/App.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Random;
 
 public class App {
+    
     public static void main(String[] args) throws Exception {
         
         List<String> habilidades = List.of("Ataque", "Pase", "Defensa");
